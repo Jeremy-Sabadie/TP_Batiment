f9ef7db305562e535e5ec7978c8575fba05fba1c
cda07saba <jeremy.sabadie@2isa.org>
2023-01-17 15:53:34 +0100

    Ajoutez des fichiers projet.

---
+++ TP_Batiment/Batiment.cs
@@ -0,0 +1,20 @@
+﻿namespace TP_Batiment
+{
+    //Attributs de la classe:
+    internal class Batiment
+    {
+        private string Adress;
+        //Constructeur1:
+        public Batiment(string adress)
+        {
+            Adress = adress;
+        }
+        //Constructeur par défault:
+        public Batiment()
+        {
+
+        }
+        //Accesseur:
+        public string Adress1 { get => Adress; set => Adress = value; }
+    }
+}
---
+++ TP_Batiment/Maison.cs
@@ -0,0 +1,15 @@
+﻿namespace TP_Batiment
+{//Classe Maison qui hérite de la classe Batiment:
+    internal class Maison : Batiment
+    {//Attribut NbRoom spécifique à la classe enfant Maison:
+        private int nbPieces;
+        public Maison(string Adress, int nbPieces)//constructeur classe (enfant)
+            : base(Adress)//constructeur classe^parente déjà typé dans la classe parente.
+        {
+
+        }
+        //Accesseur de l'attribut NbRoom:
+        public int NbRoom1 { get => nbPieces; set => nbPieces = value; }
+
+    }
+}
